21995da44f96cb22971206d62510141e131fb6fc
simyy <176912231@163.com>
2017-10-01 08:30:55 +0800

    update sql

--- db.sql
+++ db.sql
@@ -4,7 +4,7 @@ create database `Zoro`;
 # 用户 
 create table `user` (
     `id` bigint(64) not null auto_increment primary key, 
-    `unionId` varchar(256) not null comment '微信唯一ID',
+    `unionId` varchar(64) not null comment '微信唯一ID',
     `nickName` varchar(256) not null comment '用户昵称',
     `avatarUrl` varchar(1024) not null comment '用户头像',
     `gender` int(1) not null comment '性别： 0未知、1男、2女',
@@ -34,5 +34,5 @@ create table `drink_recommend` (
     `id` bigint(64) not null auto_increment primary key,
     `expireTime` datetime not null comment '过期时间',
     `gmtModify` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NULL COMMENT '更新时间', 
-    `gmtCreate` TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '注册时间',
+    `gmtCreate` TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT '注册时间'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 comment '喝水推荐';
