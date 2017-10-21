349d5cf7e5027fe886c9b5677dbf098136ad8d2a
simyy <176912231@163.com>
2017-10-21 19:16:19 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 if [[ $# == 1 && "$1" == "debug" ]]; then 
     export FLASK_CONFIG="development"
+    python manage.py runserver --port=4000
 else
     export FLASK_CONFIG="production"
+    /opt/work/zoro/bin/python manage.py runserver --port 4000
 fi
-
-python manage.py runserver --port=4000
