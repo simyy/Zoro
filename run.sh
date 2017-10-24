b37a20533e9d3197f582e0578b8f84b7ecae06bf
simyy <176912231@163.com>
2017-10-24 20:14:07 +0800

    update path

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+
 if [[ $# == 1 && "$1" == "debug" ]]; then 
     export FLASK_CONFIG="development"
     python manage.py runserver --port=4000
 else
     export FLASK_CONFIG="production"
-    /opt/work/zoro/bin/python manage.py runserver --port 4000
+    /opt/work/zoro/bin/python /opt/work/zoro/src/manage.py runserver --port 4000
 fi
