d69cc660cbd3887edc30fc0e5d6bb4137d6e8f85
simyy <176912231@163.com>
2017-12-29 22:11:48 +0800

    update

--- app/models/user.py
+++ app/models/user.py
@@ -9,6 +9,7 @@ class User(db.Model, BaseModel):
     __tablename__ = 'user'
 
     id = Column(BigInteger, primary_key=True)
+    appId = Column(String(64))
     mobile = Column(String(64))
     unionId = Column(String(256))
     nickName = Column(String(256))
@@ -19,7 +20,8 @@ class User(db.Model, BaseModel):
     country = Column(String(128))
     isDeleted = Column(Integer)
 
-    def __init__(self, unionId, nickName, avatarUrl, gender, province=None, city=None, country=None):
+    def __init__(self, appId, unionId, nickName, avatarUrl, gender, province=None, city=None, country=None):
+        self.appId = appId
         self.unionId = unionId 
         self.nickName = nickName
         self.avatarUrl = avatarUrl
